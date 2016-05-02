5a21ee0d136df323b838a33f5730ba5bb7877020
Julien Dubois <julien.dubois@gmail.com>
2016-05-02 23:41:48 +0200

    Applications and profiles are variables

--- src/main/webapp/app/config/config.controller.js
+++ src/main/webapp/app/config/config.controller.js
@@ -9,13 +9,15 @@
 
     function ConfigController (ConfigService) {
         var vm = this;
+        vm.application = "application";
+        vm.profile = "prod";
         vm.refresh = refresh;
 
         vm.refresh();
 
         function refresh () {
 
-            ConfigService.getConfig().then(function (response) {
+            ConfigService.getConfig(vm.application, vm.profile).then(function (response) {
                 vm.data = response;
             });
         }
--- src/main/webapp/app/config/config.service.js
+++ src/main/webapp/app/config/config.service.js
@@ -15,8 +15,8 @@
 
         return config;
 
-        function getConfig() {
-            return $http.get('config/application-dev.yml').then(function (response) {
+        function getConfig(application, profile) {
+            return $http.get('config/' + application + '-' + profile + '.yml').then(function (response) {
                 return response.data;
             });
         }
