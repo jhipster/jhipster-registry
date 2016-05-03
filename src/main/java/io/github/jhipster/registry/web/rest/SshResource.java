e3749609adccbec66dbfe0ee879a58cf3e255bdd
Julien Dubois <julien.dubois@gmail.com>
2016-05-03 12:53:10 +0200

    Add SSH documentation
    Fix #41

--- src/main/java/io/github/jhipster/registry/web/rest/SshResource.java
+++ src/main/java/io/github/jhipster/registry/web/rest/SshResource.java
@@ -28,11 +28,15 @@ public class SshResource {
         method = RequestMethod.GET,
         produces = MediaType.TEXT_PLAIN_VALUE)
     @Timed
-    public ResponseEntity<String> eureka() throws IOException {
-        String publicKey =
-            new String(Files.readAllBytes(
+    public ResponseEntity<String> eureka() {
+        try {
+            String publicKey = new String(Files.readAllBytes(
                 Paths.get(System.getProperty("user.home") +"/.ssh/id_rsa.pub")));
 
-        return new ResponseEntity<>(publicKey, HttpStatus.OK);
+            return new ResponseEntity<>(publicKey, HttpStatus.OK);
+        } catch (IOException e) {
+            log.warn("SSH public key could not be loaded: {}", e.getMessage());
+            return new ResponseEntity<String>("", HttpStatus.NOT_FOUND);
+        }
     }
 }
