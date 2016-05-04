3ff97e58ca59ea809247b13402563fe6ca53f957
Deepu k Sasidharan <d4udts@gmail.com>
2016-05-04 14:39:36 +0800

    add highlight js for code highlight

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/jquery/dist/jquery.js',
             'src/main/webapp/bower_components/json3/lib/json3.js',
             'src/main/webapp/bower_components/messageformat/messageformat.js',
+            'src/main/webapp/bower_components/highlightjs/highlight.pack.js',
             'src/main/webapp/bower_components/angular/angular.js',
             'src/main/webapp/bower_components/angular-aria/angular-aria.js',
             'src/main/webapp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
@@ -36,6 +37,7 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/angular-resource/angular-resource.js',
             'src/main/webapp/bower_components/angular-sanitize/angular-sanitize.js',
             'src/main/webapp/bower_components/angular-ui-router/release/angular-ui-router.js',
+            'src/main/webapp/bower_components/angular-highlightjs/build/angular-highlightjs.js',
             'src/main/webapp/bower_components/angular-mocks/angular-mocks.js',
             // endbower
             'src/main/webapp/app/app.module.js',
