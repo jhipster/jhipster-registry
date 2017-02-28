a3a9aa0fbf031d43f1c3106385f1ec3f7b14517f
Mohamed <mohamed.aitabderrahman@gmail.com>
2017-02-28 10:29:55 +0000

    change webpack.dev proxy target port

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -26,7 +26,7 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
                 '/v2/api-docs',
                 '/h2-console'
             ],
-            target: 'http://127.0.0.1:8080',
+            target: 'http://127.0.0.1:8761',
             secure: false
         }]
     },
