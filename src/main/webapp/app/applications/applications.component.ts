256a22974987a1d25c012291d0bf3fa36dc80150
Mohamed <mohamed.aitabderrahman@gmail.com>
2017-02-28 10:49:36 +0000

    add applications module

---
+++ src/main/webapp/app/applications/applications.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {JhiApplicationsService} from './applications.service';
+
+@Component({
+    selector: 'jhi-applications',
+    templateUrl: './applications.component.html',
+})
+export class JhiApplicationsComponent implements OnInit {
+    application: any;
+    data: any;
+    instances: any;
+
+    constructor(private applicationsService: JhiApplicationsService) {}
+
+    ngOnInit() {
+        this.refresh();
+    }
+
+    refresh() {
+        this.applicationsService.findAll().toPromise().then((data) => {
+            this.data = data;
+            if (data.applications.length > 0) {
+                this.show(data.applications[0].name);
+            }
+        });
+
+    }
+
+    show(app) {
+        this.application = app;
+        for (let application of this.data.applications) {
+            application.active = '';
+            if (application.name === this.application) {
+                this.instances = application.instances;
+                application.active = 'active';
+            }
+        }
+    }
+
+}
---
+++ src/main/webapp/app/applications/applications.module.ts
@@ -0,0 +1,28 @@
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {JhipsterRegistrySharedModule} from '../shared';
+
+import {
+    APPLICATIONS_ROUTE,
+    JhiApplicationsComponent,
+    JhiApplicationsService
+} from './';
+
+
+@NgModule({
+    imports: [
+        JhipsterRegistrySharedModule,
+        RouterModule.forRoot([APPLICATIONS_ROUTE], {useHash: true})
+    ],
+    declarations: [
+        JhiApplicationsComponent
+    ],
+    entryComponents: [],
+    providers: [
+        JhiApplicationsService
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class JHipsterRegistryApplicationsModule {
+}
