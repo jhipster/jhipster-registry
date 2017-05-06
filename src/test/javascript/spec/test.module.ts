3657909e4fd6881d574e62b9f3d81ec2a9dd852c
Julien Dubois <julien.dubois@gmail.com>
2017-05-07 00:14:22 +0200

    Fix unit tests

--- src/test/javascript/spec/test.module.ts
+++ src/test/javascript/spec/test.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MockBackend } from '@angular/http/testing';
 import { Http, BaseRequestOptions } from '@angular/http';
+import { SessionStorageService } from 'ng2-webstorage';
+import { JhiRefreshService } from '../../../main/webapp/app/shared/refresh/refresh.service';
 
 @NgModule({
     providers: [
         MockBackend,
+        SessionStorageService,
+        JhiRefreshService,
         BaseRequestOptions,
         {
             provide: Http,
