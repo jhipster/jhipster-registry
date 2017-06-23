6f03096d4b34db369a6423fcb73f90043ba2997f
Diego Pessoa <diegopessoa12@gmail.com>
2017-06-22 23:48:05 -0300

    Add AppInfoResource with javadoc

--- src/main/java/io/github/jhipster/registry/web/rest/AppInfoResource.java
+++ src/main/java/io/github/jhipster/registry/web/rest/AppInfoResource.java
@@ -4,14 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * Created by diego on 22/06/17.
+ * REST Controller for viewing information about the currently running application.
  */
 public class AppInfoResource {
 
     @Value("${eureka.instance.metadata-map.version}")
     private String appVersion;
 
-
+    /**
+     * GET  /info/version : get the version of the currently running application.
+     * @return the application version
+     */
     @GetMapping("/info/version")
     public String getVersion() {
         return appVersion;
