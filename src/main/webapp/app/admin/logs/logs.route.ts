dc709c8b2e5a687e23a09164800735d08d1d48e6
Pierre Besson <pbesson@ippon.fr>
2017-07-24 12:34:31 +0200

    simplify route links to clarify logs vs loggers

--- src/main/webapp/app/admin/logs/logs.route.ts
+++ src/main/webapp/app/admin/logs/logs.route.ts
@@ -3,9 +3,9 @@ import { Route } from '@angular/router';
 import { LogsComponent } from './logs.component';
 
 export const logsRoute: Route = {
-    path: 'logs',
+    path: 'loggers',
     component: LogsComponent,
     data: {
-        pageTitle: 'Logs'
+        pageTitle: 'Loggers'
     }
 };
