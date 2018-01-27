8327abc19683be7815e9a7e152ecb514b88f3d64
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-01-27 19:56:00 +0100

    Upgrade to Angular5 and some others dependencies

--- src/main/webapp/app/blocks/interceptor/http.provider.ts
+++ src/main/webapp/app/blocks/interceptor/http.provider.ts
@@ -1,9 +1,9 @@
 import { Injector } from '@angular/core';
 import { Http, XHRBackend, RequestOptions } from '@angular/http';
-import { EventManager, InterceptableHttp } from 'ng-jhipster';
+import { JhiEventManager, JhiInterceptableHttp } from 'ng-jhipster';
 
 import { AuthInterceptor } from './auth.interceptor';
-import { LocalStorageService, SessionStorageService } from 'ng2-webstorage';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { AuthExpiredInterceptor } from './auth-expired.interceptor';
 import { ErrorHandlerInterceptor } from './errorhandler.interceptor';
 import { NotificationInterceptor } from './notification.interceptor';
@@ -14,9 +14,9 @@ export function interceptableFactory(
     localStorage: LocalStorageService,
     sessionStorage: SessionStorageService,
     injector: Injector,
-    eventManager: EventManager
+    eventManager: JhiEventManager
 ) {
-    return new InterceptableHttp(
+    return new JhiInterceptableHttp(
         backend,
         defaultOptions,
         [
@@ -39,7 +39,7 @@ export function customHttpProvider() {
             LocalStorageService,
             SessionStorageService,
             Injector,
-            EventManager
+            JhiEventManager
         ]
     };
 };
