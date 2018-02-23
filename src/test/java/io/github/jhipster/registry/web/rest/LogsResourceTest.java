7b0ad0792482e33de4bf7c42861e054ff69a1def
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-02-23 23:39:48 +0100

    Fix tests

--- src/test/java/io/github/jhipster/registry/web/rest/LogsResourceTest.java
+++ src/test/java/io/github/jhipster/registry/web/rest/LogsResourceTest.java
@@ -5,7 +5,7 @@ import io.github.jhipster.registry.web.rest.vm.LoggerVM;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
