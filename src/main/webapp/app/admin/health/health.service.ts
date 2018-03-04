7fbd1317b436d555c3e1dc3df8207339f846bda4
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-03-04 22:13:48 +0100

    Clean import and use Rx/Observable

--- src/main/webapp/app/admin/health/health.service.ts
+++ src/main/webapp/app/admin/health/health.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import { Route } from '../../shared';
 
 @Injectable()
--- src/main/webapp/app/core/user/user.service.ts
+++ src/main/webapp/app/core/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 import { SERVER_API_URL } from 'app/app.constants';
 import { createRequestOption } from 'app/shared/util/request-util';
