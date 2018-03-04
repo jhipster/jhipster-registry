ef32b6e2aad5b9a19017495dfb45d7da374a5062
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-03-04 23:37:43 +0100

    Fix selector

--- src/main/webapp/app/shared/refresh/refresh-selector.component.ts
+++ src/main/webapp/app/shared/refresh/refresh-selector.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import { JhiRefreshService } from './refresh.service';
 
 @Component({
