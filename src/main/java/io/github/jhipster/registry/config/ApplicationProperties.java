c922064c3b9478f7711bd797db860ee5dc3edae2
anton <anton.barkan@segasec.com>
2019-01-27 16:37:28 +0200

    move oauth2 attributes to config

--- src/main/java/io/github/jhipster/registry/config/ApplicationProperties.java
+++ src/main/java/io/github/jhipster/registry/config/ApplicationProperties.java
@@ -12,4 +12,32 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
 
+    private final Oauth2 oauth2 = new Oauth2();
+
+    public Oauth2 getOauth2() {
+        return oauth2;
+    }
+
+    public static class Oauth2 {
+
+        private String principalAttribute;
+
+        private String authoritiesAttribute;
+
+        public String getPrincipalAttribute() {
+            return principalAttribute;
+        }
+
+        public void setPrincipalAttribute(String principalAttribute) {
+            this.principalAttribute = principalAttribute;
+        }
+
+        public String getAuthoritiesAttribute() {
+            return authoritiesAttribute;
+        }
+
+        public void setAuthoritiesAttribute(String authoritiesAttribute) {
+            this.authoritiesAttribute = authoritiesAttribute;
+        }
+    }
 }
