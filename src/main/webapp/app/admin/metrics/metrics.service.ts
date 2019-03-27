6f43b33b54dada2edc276127b4050b0f9376242c
Clément Dessoude <clement.dessoude@gmail.com>
2019-03-27 15:05:19 +0100

    fix(metrics route): fix url for calling microservices metrics

--- src/main/webapp/app/admin/metrics/metrics.service.ts
+++ src/main/webapp/app/admin/metrics/metrics.service.ts
@@ -11,13 +11,14 @@ export class JhiMetricsService {
 
     // get the Registry's metrics
     getMetrics(): Observable<any> {
+        // There is still a "-" in "/management/jhi-metrics" as the JHipster Registry don't use v3.0.0+ of the JHipster dependencies
         return this.http.get(SERVER_API_URL + 'management/jhi-metrics');
     }
 
     // get the instance's metrics
     getInstanceMetrics(instance: Route): Observable<any> {
         if (instance && instance.prefix && instance.prefix.length > 0) {
-            return this.http.get(instance.prefix + '/management/jhi-metrics');
+            return this.http.get(instance.prefix + '/management/jhimetrics');
         }
         return this.getMetrics();
     }
