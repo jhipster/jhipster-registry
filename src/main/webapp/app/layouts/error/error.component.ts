7194d9e972b578f2aacbd670a13756fbbd19f81f
Julien Sadaoui <julien.sadaoui@gmail.com>
2020-01-23 01:20:38 +0100

    Fix errors in production build

--- src/main/webapp/app/layouts/error/error.component.ts
+++ src/main/webapp/app/layouts/error/error.component.ts
@@ -7,11 +7,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ErrorComponent implements OnInit {
   errorMessage?: string;
+  error403?: boolean;
+  error404?: boolean;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.data.subscribe(routeData => {
+      if (routeData.error403) {
+        this.error403 = routeData.error403;
+      }
+      if (routeData.error404) {
+        this.error404 = routeData.error404;
+      }
       if (routeData.errorMessage) {
         this.errorMessage = routeData.errorMessage;
       }
