c3a41d021c40b8ed795b1c701f4b0930e11ec6f2
Julien Sadaoui <julien.sadaoui@gmail.com>
2022-12-24 11:41:24 +0100

    Disable JHipster Control Center Support

---
+++ src/main/webapp/app/admin/health/health.model.ts
@@ -0,0 +1,24 @@
+export type HealthStatus = 'UP' | 'DOWN' | 'UNKNOWN' | 'OUT_OF_SERVICE';
+
+export type HealthKey =
+  | 'discoveryComposite'
+  | 'refreshScope'
+  | 'clientConfigServer'
+  | 'hystrix'
+  | 'diskSpace'
+  | 'mail'
+  | 'ping'
+  | 'livenessState'
+  | 'readinessState';
+
+export interface Health {
+  status: HealthStatus;
+  components: {
+    [key in HealthKey]?: HealthDetails;
+  };
+}
+
+export interface HealthDetails {
+  status: HealthStatus;
+  details?: { [key: string]: unknown };
+}
